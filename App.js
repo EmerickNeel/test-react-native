c51d1eae117952c2a5424dfaebd051d7ff3dbe39
Emerick NEEL <emerickneel@gmail.com>
2019-11-07 17:37:24 +0100

    TP

--- App.js
+++ App.js
@@ -1,11 +1,175 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+
+class Ecran extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.ecran}>
+        <Text style={{ color: "white", top: 70, fontSize: 20 }}>{this.props.label}</Text>
+      </View>
+    );
+  }
+}
+
+class Calc extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    label: ''
+  }
+
+  render() {
+    return (
+      <View style={styles.base}>
+        <Ecran label={this.state.label}></Ecran>
+        <View style={{ flex: 3, flexDirection: "row" }}>
+          <View style={{ flex: 1 }}>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={() => {
+                this.setState({
+                  label : this.state.label + "1"
+                })
+              }}>
+                <Text style={styles.btnText}>Press</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>(</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>7</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>4</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>1</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>0</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={{ flex: 1 }}>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>Clean</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>)</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>8</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>5</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>2</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>00</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+          <View style={{ flex: 1 }}>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>Del</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>%</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>9</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>6</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>3</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnChiffre}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>.</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>/</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>X</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>-</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnOpe}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>+</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.btnEgale}>
+              <TouchableOpacity onPress={this._onPressButton}>
+                <Text style={styles.btnText}>=</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Calc></Calc>
   );
 }
 
@@ -16,4 +180,47 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  base: {
+    flex: 1,
+  },
+
+  ecran: {
+    flex: 2,
+    backgroundColor: "#151925",
+    right: 0,
+    alignItems: "flex-end"
+  },
+
+  btnOpe: {
+    flex: 1,
+    backgroundColor: "#252E3E",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+
+  btnChiffre: {
+    flex: 1,
+    backgroundColor: "#343C4D",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+
+  btnEgale: {
+    flex: 2,
+    backgroundColor: "#52AB5E",
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+
+  btnText: {
+    color: "white",
+    fontWeight: "bold"
+  }
 });
